de6daf32d5bc149b64f8debc09d627e4c97a4a54
3340398079 <3340398079@qq.com>
2017-06-02 11:32:25 +0800

    Bare Repository

---
+++ app/src/main/java/com/aces/pillow/camest/CamActivity.java
@@ -0,0 +1,13 @@
+package com.aces.pillow.camest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CamActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cam);
+    }
+}
