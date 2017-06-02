fedde851b1d30eb5c68a2c59d0434a2cf3d223d8
3340398079 <3340398079@qq.com>
2017-06-03 00:08:18 +0800

    Face and Shoulder Detection

--- app/src/main/java/com/aces/pillow/camest/CamActivity.java
+++ app/src/main/java/com/aces/pillow/camest/CamActivity.java
@@ -1,13 +1,173 @@
 package com.aces.pillow.camest;
 
-import android.app.Activity;
-import android.os.Bundle;
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewFrame;
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
+import org.opencv.android.LoaderCallbackInterface;
 
-public class CamActivity extends Activity {
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Rect;
+
+import org.opencv.core.Mat;
+import org.opencv.core.Scalar;
+
+import org.opencv.imgproc.Imgproc;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.WindowManager;
+
+public class CamActivity extends Activity implements CvCameraViewListener2 {
+
+    private static final String    TAG                 = CamActivity.class.getName();
+
+    private static final Scalar    FACE_RECT_COLOR     = new Scalar(0, 255, 0, 127);
+
+    private static final int      thickness = 2;
+
+    private static final int       MAXIMUM_ALLOWED_SKIPPED_FRAMES = 15;
+
+    private int                      count = 0;
+
+    private double                   width = 0;
+
+    private CameraBridgeViewBase   mOpenCvCameraView;
+
+    private Mat                    mRgba;
+    private Mat                    mGray;
+
+    private Detector               mDetector;
+
+    private Rect                   face_frame;
+    private Rect                   shoulder_frame;
+
+    private BaseLoaderCallback  mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                case LoaderCallbackInterface.SUCCESS:
+                {
+                    Log.i(TAG, "OpenCV loaded successfully");
+
+                    mDetector.load();
+
+                    mOpenCvCameraView.setCameraIndex(1);
+                    mOpenCvCameraView.enableView();
+                } break;
+                default:
+                {
+                    super.onManagerConnected(status);
+                } break;
+            }
+        }
+    };
+
+    public CamActivity() {
+        Log.i(TAG, "Instantiated new " + this.getClass());
+    }
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        Log.i(TAG, "called onCreate");
+        super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        setContentView(R.layout.activity_cam);
+
+        mDetector = new Detector(this);
+
+        mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.cam_activity_surface_view);
+        mOpenCvCameraView.setVisibility(CameraBridgeViewBase.VISIBLE);
+        mOpenCvCameraView.setCvCameraViewListener(this);
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cam);
+    public void onPause()
+    {
+        super.onPause();
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView.disableView();
     }
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        if (!OpenCVLoader.initDebug()) {
+            Log.d(TAG, "Internal OpenCV library not found. Using OpenCV Manager for initialization");
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_0_0, this, mLoaderCallback);
+        } else {
+            Log.d(TAG, "OpenCV library found inside package. Using it!");
+            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        }
+    }
+
+    public void onDestroy() {
+        super.onDestroy();
+        mOpenCvCameraView.disableView();
+    }
+
+    public void onCameraViewStarted(int width, int height) {
+        mGray = new Mat();
+        mRgba = new Mat();
+
+        face_frame = new Rect();
+        shoulder_frame = new Rect();
+    }
+
+    public void onCameraViewStopped() {
+        mGray.release();
+        mRgba.release();
+    }
+
+    public Mat onCameraFrame(CvCameraViewFrame inputFrame) {
+
+        mRgba = inputFrame.rgba();
+        mGray = inputFrame.gray();
+
+        if (count == 0) {
+            face_frame = mDetector.FaceDetector(mGray);
+        } else {
+            show_face();
+        }
+
+        if (mDetector.isFaceDetected()) {
+            if (count > MAXIMUM_ALLOWED_SKIPPED_FRAMES) {
+                count = 0;
+                mDetector.setFaceDetected(false);
+            } else {
+                shoulder_frame = mDetector.ShoulderDetector(mGray);
+                show_face();
+                if (mDetector.isShoulderDetected() &&
+                        (shoulder_frame.x < face_frame.x) && (shoulder_frame.width > face_frame.width)) {
+                    width = (shoulder_frame.width - face_frame.width) / 2;
+                    show_shoulder();
+                }
+            }
+
+        }
+
+        return mRgba;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    private void show_face() {
+        if (face_frame != null) {
+            Imgproc.rectangle(mRgba, face_frame.tl(), face_frame.br(), FACE_RECT_COLOR, thickness);
+            count++;
+        }
+    }
+
+    private void show_shoulder() {
+        if (shoulder_frame != null)
+            Imgproc.rectangle(mRgba, shoulder_frame.tl(), shoulder_frame.br(), FACE_RECT_COLOR, thickness);
+    }
+
 }
---
+++ app/src/main/java/com/aces/pillow/camest/Detector.java
@@ -0,0 +1,222 @@
+package com.aces.pillow.camest;
+
+import android.content.Context;
+import android.util.Log;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.opencv.objdetect.CascadeClassifier;
+import org.opencv.objdetect.Objdetect;
+
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfRect;
+import org.opencv.core.Rect;
+import org.opencv.core.Size;
+
+/**
+ * Created by Administrator on 2017/6/2 0002.
+ */
+
+public class Detector {
+
+    private static Context context;
+
+    private static final String    TAG                 = Detector.class.getName();
+
+    public static final int        JAVA_DETECTOR       = 0;
+
+    private int                     mDetectorType       = JAVA_DETECTOR;
+
+    private File                    mCascadeFile;
+    private CascadeClassifier       mJavaDetector;
+
+    private File                    mCascadeFileEye;
+    private File                    mCascadeFileShoulder;
+
+    private CascadeClassifier       mJavaDetectorEye;
+    private CascadeClassifier       mJavaDetectorShoulder;
+
+    private float                  mRelativeFaceSize   = 0.2f;
+    private int                    mAbsoluteFaceSize   = 0;
+
+    private boolean               FaceDetected = false;
+    private boolean               ShoulderDetected = false;
+
+    public Detector(Context curr) {
+        context = curr;
+    }
+
+    public void load() {
+        try {
+            // load cascade file from application resources
+            InputStream is = context.getResources().openRawResource(R.raw.haarcascade_frontalface_default);
+            File cascadeDir = context.getDir("cascade", Context.MODE_PRIVATE);
+            mCascadeFile = new File(cascadeDir, "haarcascade_frontalface_default.xml");
+            FileOutputStream os = new FileOutputStream(mCascadeFile);
+
+            byte[] buffer = new byte[4096];
+            int bytesRead;
+            while ((bytesRead = is.read(buffer)) != -1) {
+                os.write(buffer, 0, bytesRead);
+            }
+            is.close();
+            os.close();
+
+            mJavaDetector = new CascadeClassifier(mCascadeFile.getAbsolutePath());
+            mJavaDetector.load(mCascadeFile.getAbsolutePath());
+            if (mJavaDetector.empty()) {
+                Log.e(TAG, "Failed to load cascade classifier");
+                mJavaDetector = null;
+            } else
+                Log.i(TAG, "Loaded cascade classifier from " + mCascadeFile.getAbsolutePath());
+
+            // load cascade file from application resources
+            InputStream isEYE = context.getResources().openRawResource(R.raw.haarcascade_eye_tree_eyeglasses);
+            File cascadeDirEYE = context.getDir("cascadeEYE", Context.MODE_PRIVATE);
+            mCascadeFileEye = new File(cascadeDirEYE, "haarcascade_eye_tree_eyeglasses.xml");
+            FileOutputStream osEYE = new FileOutputStream(mCascadeFileEye);
+
+            byte[] bufferEYE = new byte[4096];
+            int bytesReadEYE;
+            while ((bytesReadEYE = isEYE.read(bufferEYE)) != -1) {
+                osEYE.write(bufferEYE, 0, bytesReadEYE);
+            }
+            isEYE.close();
+            osEYE.close();
+
+            mJavaDetectorEye = new CascadeClassifier(mCascadeFileEye.getAbsolutePath());
+            mJavaDetectorEye.load(mCascadeFileEye.getAbsolutePath());
+            if (mJavaDetectorEye.empty()) {
+                Log.e(TAG, "Failed to load cascade classifier");
+                mJavaDetectorEye = null;
+            } else
+                Log.i(TAG, "Loaded cascade classifier for eye from " + mCascadeFile.getAbsolutePath());
+
+            // load cascade file from application resources
+            InputStream isShoulder = context.getResources().openRawResource(R.raw.haarcascade_mcs_upperbody);
+            File cascadeDirShoulder = context.getDir("cascadeShoulder", Context.MODE_PRIVATE);
+            mCascadeFileShoulder = new File(cascadeDirShoulder, "haarcascade_mcs_upperbodyy.xml");
+            FileOutputStream osShoulder = new FileOutputStream(mCascadeFileShoulder);
+
+            byte[] bufferShoulder = new byte[4096];
+            int bytesReadShoulder;
+            while ((bytesReadShoulder = isShoulder.read(bufferShoulder)) != -1) {
+                osShoulder.write(bufferShoulder, 0, bytesReadShoulder);
+            }
+            isShoulder.close();
+            osShoulder.close();
+
+            mJavaDetectorShoulder = new CascadeClassifier(mCascadeFileShoulder.getAbsolutePath());
+            mJavaDetectorShoulder.load(mCascadeFileShoulder.getAbsolutePath());
+            if (mJavaDetectorShoulder.empty()) {
+                Log.e(TAG, "Failed to load cascade classifier");
+                mJavaDetectorShoulder = null;
+            } else
+                Log.i(TAG, "Loaded cascade classifier for upper body from " + mCascadeFile.getAbsolutePath());
+
+            cascadeDir.delete();
+            cascadeDirEYE.delete();
+            cascadeDirShoulder.delete();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            Log.e(TAG, "Failed to load cascade. Exception thrown: " + e);
+        }
+    }
+
+    public boolean isFaceDetected() {
+        return FaceDetected;
+    }
+
+    public boolean isShoulderDetected() {
+        return ShoulderDetected;
+    }
+
+    public void setFaceDetected(boolean b) {
+        FaceDetected = b;
+    }
+
+    public void setShoulderDetected(boolean b) {ShoulderDetected = b;}
+
+    public Rect FaceDetector (Mat gray) {
+
+        MatOfRect faces = new MatOfRect();
+        MatOfRect eyes = new MatOfRect();
+
+        if (mAbsoluteFaceSize == 0) {
+            int height = gray.rows();
+            if (Math.round(height * mRelativeFaceSize) > 0) {
+                mAbsoluteFaceSize = Math.round(height * mRelativeFaceSize);
+            }
+        }
+
+        if (mDetectorType == JAVA_DETECTOR) {
+            if (mJavaDetector != null)
+                mJavaDetector.detectMultiScale(gray, faces, 1.1, 2, 2, // TODO: objdetect.CV_HAAR_SCALE_IMAGE
+                        new Size(mAbsoluteFaceSize, mAbsoluteFaceSize), new Size());
+        }
+        else {
+            Log.e(TAG, "Detection method is not selected!");
+            return null;
+        }
+
+        Rect[] facesArray = faces.toArray();
+        for (int i = 0; i < facesArray.length; i++) {
+
+            Mat mFace = gray.submat(facesArray[i]);
+
+            if (mJavaDetectorEye != null) {
+                mJavaDetectorEye.detectMultiScale(mFace, eyes, 1.1, 2,
+                        Objdetect.CASCADE_SCALE_IMAGE, new Size(0, 0), new Size());
+                if (!eyes.empty()) {
+                    FaceDetected = true;
+                    // Face_skip_frame = 1;
+                    return facesArray[i];
+                }
+            }
+        }
+
+        return null;
+    }
+
+    public Rect ShoulderDetector (Mat gray) {
+
+        MatOfRect shoulders = new MatOfRect();
+        MatOfRect eyes = new MatOfRect();
+
+        if (mAbsoluteFaceSize == 0) {
+            int height = gray.rows();
+            if (Math.round(height * mRelativeFaceSize) > 0) {
+                mAbsoluteFaceSize = Math.round(height * mRelativeFaceSize);
+            }
+        }
+
+        if (mDetectorType == JAVA_DETECTOR) {
+            if (mJavaDetectorShoulder != null)
+                mJavaDetectorShoulder.detectMultiScale(gray, shoulders, 1.1, 2, 2, // TODO: objdetect.CV_HAAR_SCALE_IMAGE
+                        new Size(mAbsoluteFaceSize, mAbsoluteFaceSize), new Size());
+        }
+        else {
+            Log.e(TAG, "Detection method is not selected!");
+            return null;
+        }
+
+        int max_idx = 0;
+        if (!shoulders.empty()) {
+            Rect[] shouldersArray = shoulders.toArray();
+            for (int i = 1; i < shouldersArray.length; i++) {
+
+                if (shouldersArray[i].width > shouldersArray[max_idx].width)
+                    max_idx = i;
+            }
+
+            ShoulderDetected = true;
+            return shouldersArray[max_idx];
+        }
+
+        return null;
+    }
+}
